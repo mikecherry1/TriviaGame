c7d20ecbacbf640f1d84fc43a89b1d97c364ec26
mikecherry1 <mikecherry1@gmail.com>
2017-06-20 01:39:06 -0400

    almost complete files without trivia questions added

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+$('#start').on('click', function() {
+	game.start();
+})
+
+$(document).on('click', '#end',function() {
+	game.done();
+
+});
+
+var questions = [{
+	question: "What was",
+	answers: ["answer 1, "anßswer 2", ]
+}]
+});
